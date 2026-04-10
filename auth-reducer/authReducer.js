16b890ac5df0dd6fca2015ffd5cfd5bf6c5f526c
AnjaliMore1421 <moreanjali219@gmail.com>
2026-04-10 14:47:36 +0530

    Create authReducer.js

---
+++ auth-reducer/authReducer.js
@@ -0,0 +1,25 @@
+export const initialState = {
+  isLoggedIn: false,
+  user: null
+};
+
+export function authReducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: action.payload
+      };
+
+    case "LOGOUT":
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null
+      };
+
+    default:
+      return state;
+  }
+}
