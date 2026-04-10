30f90a1eea5cacbcf2286f40ba985f360b4c2109
AnjaliMore1421 <moreanjali219@gmail.com>
2026-04-10 14:35:25 +0530

    Create index.js

---
+++ user-session-management/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import SessionProvider from "./context/SessionContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <SessionProvider>
+    <App />
+  </SessionProvider>
+);
